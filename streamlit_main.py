d7c18c3a6c8b21638fa936d5f76cac41cdf47b2c
TReV-89 <71920470+TReV-89@users.noreply.github.com>
2025-06-13 23:35:23 +0300

    Update streamlit_main.py

--- streamlit_main.py
+++ streamlit_main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 import pandas as pd
 
-FASTAPI_BACKEND_URL = "http://127.0.0.1:8000"
+FASTAPI_BACKEND_URL = "https://advance-loan-calc-back-end.onrender.com/"
 #FASTAPI_BACKEND_URL = "http://backend:8000"
 
 
